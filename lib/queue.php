70a1900e65c401f9b543cd66e5c9ff59972337fe
Sungkeun Cho <skcho@ropas.snu.ac.kr>
2015-07-17 16:51:29 +0900

    Add queue

---
+++ lib/queue.php
@@ -0,0 +1,23 @@
+<?php
+
+function read_queue($file){
+  $ids = explode("\n", file_get_contents($file));
+  $ids = array_filter($ids,
+                      function($id){
+                        if($id === "") return false;
+                        else return true;
+                      });
+  return $ids;
+}
+
+function pop_and_push(&$queue, $num){
+  $elts = array();
+  for($i = 0; $i < $num; $i++){
+    $elt = array_shift($queue);
+    array_push($queue, $elt);
+    array_push($elts, $elt);
+  }
+  return $elts;
+}
+
+?>
