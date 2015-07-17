6ebefd44ca7fe73e2ca14c994f8a4eca5ca59825
Sungkeun Cho <skcho@ropas.snu.ac.kr>
2015-07-17 17:32:32 +0900

    Rename "talks" to "snts"

--- lib/schedule.php
+++ lib/schedule.php
@@ -16,13 +16,13 @@ function is_day($d, $day){
   else return false;
 }
 
-function get_datault_talks($default, $n){
-  $talks = array();
+function get_default_snts($default, $n){
+  $snts = array();
   $d = date('Y-m-d');
   while($n > 0){
     $d = next_day($d);
     if(is_day($d, $default["when"]["day"])){
-      $talk = array(
+      $snt = array(
         "when" => array(
           "year" => (int)date('Y', strtotime($d)),
           "month" => (int)date('m', strtotime($d)),
@@ -33,66 +33,66 @@ function get_datault_talks($default, $n){
         "where" => $default["where"],
         "who" => $default["who"],
       );
-      array_push($talks, $talk);
+      array_push($snts, $snt);
       $n = $n - 1;
     }
   }
-  return $talks;
+  return $snts;
 }
 
-function add_talk($talks, $exc){
-  $talk = array(
+function add_snt($snts, $exc){
+  $snt = array(
     "when" => $exc["when"],
     "where" => $exc["where"],
     "who" => $exc["who"],
   );
-  array_push($talks, $talk);
-  return $talks;
+  array_push($snts, $snt);
+  return $snts;
 }
 
-function remove_talk($talks, $exc){
-  $talks = array_filter($talks,
-                        function($talk) use($exc){
-                          if($talk["when"] === $exc["when"]) return false;
+function remove_snt($snts, $exc){
+  $snts = array_filter($snts,
+                        function($snt) use($exc){
+                          if($snt["when"] === $exc["when"]) return false;
                           else return true;
                         });
-  return $talks;
+  return $snts;
 }
 
-function modify_talk($talks, $exc){
-  $talks = remove_talk($talks, array("when" => $exc["from"]));
-  $talks = add_talk($talks, $exc);
-  return $talks;
+function modify_snt($snts, $exc){
+  $snts = remove_snt($snts, array("when" => $exc["from"]));
+  $snts = add_snt($snts, $exc);
+  return $snts;
 }
 
-function apply_exception($talks, $excs){
+function apply_exception($snts, $excs){
   foreach($excs as $exc){
     if($exc["mode"] === "add"){
-      $talks = add_talk($talks, $exc);
+      $snts = add_snt($snts, $exc);
     }else if($exc["mode"] === "remove"){
-      $talks = remove_talk($talks, $exc);
+      $snts = remove_snt($snts, $exc);
     }else if($exc["mode"] === "modify"){
-      $talks = modify_talk($talks, $exc);
+      $snts = modify_snt($snts, $exc);
     }else{
       my_log(__FILE__, "Invalid mode name in exception\n");
       exit(1);
     }
   }
-  sort($talks);
-  return $talks;
+  sort($snts);
+  return $snts;
 }
 
-function is_after_today($talk){
+function is_after_today($snt){
   $tomorrow = strtotime('+1 day');
-  $talk_day = strtotime( $talk["when"]["year"] . "-" . $talk["when"]["month"]
-                       . "-" . $talk["when"]["day"] );
-  if($talk_day >= $tomorrow) return true;
+  $snt_day = strtotime( $snt["when"]["year"] . "-" . $snt["when"]["month"]
+                       . "-" . $snt["when"]["day"] );
+  if($snt_day >= $tomorrow) return true;
   else return false;
 }
 
-function set_speaker($talk, &$queue_all, &$queue_ropas, &$queue_sf){
+function set_speaker($snt, &$queue_all, &$queue_ropas, &$queue_sf){
   $speaker = array();
-  foreach($talk["who"] as $grp => $num){
+  foreach($snt["who"] as $grp => $num){
     if($grp === "all"){
       $speaker = array_merge($speaker, pop_and_push($queue_all, $num));
     }else if($grp === "ropas"){
@@ -105,31 +105,31 @@ function set_speaker($talk, &$queue_all, &$queue_ropas, &$queue_sf){
     }
   }
 
-  $talk["who"] = $speaker;
-  return $talk;
+  $snt["who"] = $speaker;
+  return $snt;
 }
 
-function set_speakers($talks){
+function set_speakers($snts){
   $queue_all = read_queue(__ROOT__ . "/conf/queue.all");
   $queue_ropas = read_queue(__ROOT__ . "/conf/queue.ropas");
   $queue_sf = read_queue(__ROOT__ . "/conf/queue.sf");
 
-  $iter = function($talk) use(&$queue_all, &$queue_ropas, &$queue_sf){
-    return set_speaker($talk, $queue_all, $queue_ropas, $queue_sf);
+  $iter = function($snt) use(&$queue_all, &$queue_ropas, &$queue_sf){
+    return set_speaker($snt, $queue_all, $queue_ropas, $queue_sf);
   };
-  $talks = array_map($iter, $talks);
-  return $talks;
+  $snts = array_map($iter, $snts);
+  return $snts;
 }
 
-/* Return following 10 talks after today */
+/* Return following 10 S&Ts from tomorrow */
 function get_schedule(){
   $conf = json_decode(file_get_contents(__ROOT__ . "/conf/info.json"), true);
-  $talks = get_datault_talks($conf["default"], 30);
-  $talks = apply_exception($talks, $conf["exception"]);
-  $talks = array_filter($talks, "is_after_today");
-  $talks = array_slice($talks, 0, 10);
-  $talks = set_speakers($talks);
-  return $talks;
+  $snts = get_default_snts($conf["default"], 30);
+  $snts = apply_exception($snts, $conf["exception"]);
+  $snts = array_filter($snts, "is_after_today");
+  $snts = array_slice($snts, 0, 10);
+  $snts = set_speakers($snts);
+  return $snts;
 }
 
 ?>
