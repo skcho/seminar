cb61a3ccbe53ce4f8b7d0c40085ac08d7a62b6ba
Sungkeun Cho <skcho@ropas.snu.ac.kr>
2015-07-18 02:25:08 +0900

    Add send_mail library

---
+++ lib/send_mail.php
@@ -0,0 +1,24 @@
+<?php
+
+/* Send mail
+
+ - $mode: 'plain' or 'html'
+ - $from: string of mail address
+ - $to: array of strings of mail addresses
+ - $subj: UTF-8 formatted subject
+ - $msg: UTF-8 formatted mail body
+ */
+function send_mail($mode, $from, $to, $subj, $msg){
+  $headers  = 'MIME-Version: 1.0' . "\r\n";
+  $headers .= 'Content-type: text/' . $mode . '; charset=UTF-8' . "\r\n";
+  $headers .= "From: " . $from . "\r\n";
+  $subj = "=?UTF-8?B?".base64_encode($subj)."?=";
+  if(mail(implode(",", $to), $subj, $msg, $headers)){
+    my_log(__FILE__, "Succeed send notice mail");
+  }else{
+    my_log(__FILE__, "Fail send notice mail");
+    exit(1);
+  }
+}
+
+?>
