01f80dbb2657785c56c11c0c5d3fdccc858116e0
Jonggwon Kim <jgkim@ropas.snu.ac.kr>
2015-07-21 18:04:06 +0900

    Modified html style

--- www/index.php
+++ www/index.php
@@ -1,5 +1,5 @@
 <?php $title = "쇼앤텔 관리 시스템"; require 'header_noback.temp'; ?>
-
+<div id="snt-schedule">
 <h2>일정 보기 및 등록</h2>
 
 <ul>
@@ -7,7 +7,8 @@
   <li><a href="reg_abstract">발표 정보 등록 (제목/요약/메모)</a></li>
   <li><a href="reg_comment">코멘트 등록</a></li>
 </ul>
-
+</div>
+<div id="mail-schedule">
 <h2>알림 메일 발송 일정</h2>
 
 <ul>
@@ -17,5 +18,5 @@
   <li>2일 전 9시, 두 번째 알림 메일 발송</li>
   <li>1일 전 18시, 코멘트 메일 발송</li>
 </ul>
-
+</div>
 <?php require 'footer.temp'; ?>
