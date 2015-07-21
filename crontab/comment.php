4fadd1642e82e900410aea86c8284d4a0d82646a
Sungkeun Cho <skcho@ropas.snu.ac.kr>
2015-07-21 16:26:33 +0900

    Add comment viewer

--- crontab/comment.php
+++ crontab/comment.php
@@ -18,36 +18,13 @@ require_once __ROOT__ . "/lib/read_data.php";
 require_once __ROOT__ . "/lib/replace.php";
 require_once __ROOT__ . "/lib/schedule.php";
 require_once __ROOT__ . "/lib/send_mail.php";
+require_once __ROOT__ . "/lib/comment.php";
 
-
-function get_coments($when, $id){
-  $talk_data = get_talk_data(time_of_when($when), $id);
-  return $talk_data["comments"];
-}
-
-function html_of_comment($comment){
-  return "<pre style=\"line-height: 160%; font-family: Menlo, Consolas, 'Courier New', monospace; display: block; background-color: #f7f7f9; padding: 9.5px; margin: 0 0 10px; word-break: break-all; word-wrap: break-word; white-space: pre; white-space: pre-wrap; -webkit-border-radius: 4px; -moz-border-radius: 4px; border-radius: 4px; border: 1px solid #e1e1e8;\">" . htmlspecialchars($comment) . "</pre>";
-}
-
-function html_of_comments($comments){
-  return implode("\n", array_map("html_of_comment", $comments));
-}
-
-function html_of_speaker($speaker){
-  $name = get_member_name($speaker);
-  return "<h1 style=\"font-size: 1.5em;\">Comments for $name</h1>";
-}
-
-function gen_msg($snt){
-  $msg_arr = array();
-  foreach($snt["who"] as $id){
-    $msg_arr[$id] = get_coments($snt["when"], $id);
-  }
-
+function gen_msgs($snt){
+  $t = time_of_when($snt["when"]);
   $msg = "";
-  foreach($msg_arr as $speaker => $comments){
-    $msg .= html_of_speaker($speaker) . "\n";
-    $msg .= html_of_comments($comments) . "\n";
+  foreach($snt["who"] as $id){
+    $msg .= gen_msg($t, $id);
   }
   return $msg;
 }
@@ -58,17 +35,16 @@ function send($msg){
            , '우리들의 코멘트(comments on S&T memos)', $msg );
 }
 
-
 function auto(){
   $snts = snts_n_days_later(1);
   foreach($snts as $snt){
-    $msg = gen_msg($snt);
+    $msg = gen_msgs($snt);
     send($msg);
   }
 }
 
 function confirm_notice($snt){
-  $msg = gen_msg($snt);
+  $msg = gen_msgs($snt);
   echo_msg($msg);
   $confirm = ask_y_or_n("Are you sure to send the above message?");
   if($confirm){
---
+++ www/view.php
@@ -0,0 +1,20 @@
+<?php $title = "코멘트 보기"; require 'header.temp'; ?>
+
+<?php
+if(!defined('__ROOT__'))
+  define('__ROOT__', realpath(dirname(dirname(__FILE__))));
+
+require_once __ROOT__ . "/lib/interactive.php";
+require_once __ROOT__ . "/lib/log.php";
+require_once __ROOT__ . "/lib/read_data.php";
+require_once __ROOT__ . "/lib/replace.php";
+require_once __ROOT__ . "/lib/schedule.php";
+require_once __ROOT__ . "/lib/send_mail.php";
+require_once __ROOT__ . "/lib/comment.php";
+
+$id = $_REQUEST["id"];
+$t = strtotime($_REQUEST["date"]);
+echo gen_msg($t, $id);
+?>
+
+<?php require 'footer.temp'; ?>
