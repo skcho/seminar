790ab9f36631a45983679623c903cf8cedf98af7
Show N Tell Allimi <seminarbot@ropas.snu.ac.kr>
2015-07-23 10:38:11 +0900

    Revise comment on the front page

--- www/index.php
+++ www/index.php
@@ -18,13 +18,16 @@ require (__ROOT__ . '/template/header_noback.temp');
 <div class="section">
 <h2>알림 메일 발송 일정</h2>
 
+<p>쇼앤텔 발표 날짜 기준으로,</p>
+
 <ul>
-  <li>11일 전 9시, 쇼앤텔 리마인드 메일 발송</li>
-  <li>7일 전 18시, 코멘터 알림 메일 발송</li>
-  <li>6일 전 0시, 첫 번째 알림 메일 발송</li>
-  <li>2일 전 9시, 두 번째 알림 메일 발송</li>
-  <li>1일 전 18시, 코멘트 메일 발송</li>
+  <li>D-11 09:00, 발표자 리마인드</li>
+  <li>D-7 18:00, 코멘터 선정</li>
+  <li>D-6 00:00, 발표 정보 (제목/요약)</li>
+  <li>D-2 09:00, 발표 정보 (제목/요약/메모)</li>
+  <li>D-1 18:00, 코멘트 내용</li>
 </ul>
+
 </div>
 
 <?php require __ROOT__ . '/template/footer.temp'; ?>
